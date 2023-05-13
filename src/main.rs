39829a9c1c79bfd9a3b1123575932d283e5f5ebb
yanknvim <yanknvim@gmail.com>
2023-05-13 17:44:37 +0900

    Add REPL and file read

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,42 @@ mod interpreter;
 
 use std::fs::read_to_string;
 use std::env;
+use std::io::{stdout, stdin, Write};
+
+fn repl() {
+    println!("Welcome to the brainfuck REPL, type 'exit' to exit");
+    loop {
+        print!("> ");
+        stdout().flush().unwrap();
+
+        let input = {
+            let mut buffer = String::new();
+            match stdin().read_line(&mut buffer) {
+                Ok(_) => {},
+                Err(_) => panic!("Error: failed to read input"),
+            }
+            buffer
+        };
+        let input = input.trim_end();
+
+        match &*input {
+            "exit" => return,
+            _ => {
+                let tokens = interpreter::lex(&input);
+                interpreter::run(tokens);
+            },
+        }
+        println!("");
+    }
+}
 
 fn main() {
-    let code = ">++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+."; // Hello world
-
-    let tokens = interpreter::lex(code);
-    interpreter::run(tokens);
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+       repl(); 
+    } else {
+        let code = read_to_string(&args[1]).unwrap();
+        let tokens = interpreter::lex(&code);
+        interpreter::run(tokens);
+    }
 }
